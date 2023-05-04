fc071439978a703bb3379cd7eb533c3a1b8e0ed2
Wilson Chan <12698201+wilsonc0913@users.noreply.github.com>
2023-05-04 21:17:43 +0800

    my first python

---
+++ osremove.py
@@ -0,0 +1,14 @@
+import os
+print(os.getcwd())
+dir = "myDir"
+file = "myFile.txt"
+if os.path.exists(file):
+    os.remove(file)
+else:
+    print("File not exists")
+
+if not os.path.exists(dir):
+    os.mkdir(dir)
+else:
+    print("Dir exists")
+    os.rmdir(dir)
